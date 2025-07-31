35dbc2413ced09d003e595ef68bfa001da8e4a60
dheWeb <interns.dhe@gmail.com>
2025-07-31 11:36:38 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
         {children}
         <BottomView />
 
-        <Modal isOpen={isModalOpen} onClose={closeModal}>
+{/*         <Modal isOpen={isModalOpen} onClose={closeModal}>
         <div className="text-white p-4 rounded-lg flex flex-col items-center justify-center text-center text-base md:text-xl font-semibold bg-primary">
             <p>
           <strong>शिक्षा महाकुंभ 2.0</strong>was successfully concluded at Kurukshetra University from December 16-17, 2024. <strong> To download photographs click here</strong>:
@@ -109,7 +109,7 @@ export default function RootLayout({
             </p>
           </div>
 
-        </Modal>
+        </Modal> */}
         <NextTopLoader
           color="#F44336"
           initialPosition={0.08}
